2dc72b46771d23480ac4e76eed0571fd7df08e02
Bob Feng <bob.feng.com@gmail.com>
2018-11-06 20:22:08 -0800

    first commit, created index and folders

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// window.onload = function() {
+//  var vid = document.getElementById("myVideo");
+//  vid.playbackRate = 0.75;
+// }
+
+  
+$( document ).ready(function() {
+
+  // do something
+
+});
+
+
+
+
+
+
+
+
+
